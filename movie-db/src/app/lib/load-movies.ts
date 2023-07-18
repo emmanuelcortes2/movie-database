8e2c5418b25abe9c3126f14f77faf27368ad8ef7
emmanuelcortesniku <120745161+emmanuelcortesniku@users.noreply.github.com>
2023-07-17 18:24:28 -0600

    UI & functions refactoring

---
+++ movie-db/src/app/lib/load-movies.ts
@@ -0,0 +1,18 @@
+const API_KEY = "f1d951a886af6d1ecdc8225b592155f9";
+
+export async function nowPlaying() {
+  const url =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWQ5NTFhODg2YWY2ZDFlY2RjODIyNWI1OTIxNTVmOSIsInN1YiI6IjY0YjJkYjNkYzhhMmQ0MDEwMDFlNmVjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B04AVdWtY9TeKPLZj_nQ1R8rQlGcU-5oEfKR-1wYXr",
+    },
+  };
+
+  const res = await fetch(url, options);
+
+  return res.json();
+}
--- movie-db/src/components/movies_grid.tsx
+++ movie-db/src/components/movies_grid.tsx
@@ -1,35 +1,31 @@
 import Link from "next/link";
+import { nowPlaying } from "@/app/lib/load-movies";
 
 type Movie = {
   original_title: string;
   poster_path: string;
   release_date: string;
+  id: string | number
 };
 
 export default async function MovieCard() {
-  const url =
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=f1d951a886af6d1ecdc8225b592155f9";
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWQ5NTFhODg2YWY2ZDFlY2RjODIyNWI1OTIxNTVmOSIsInN1YiI6IjY0YjJkYjNkYzhhMmQ0MDEwMDFlNmVjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B04AVdWtY9TeKPLZj_nQ1R8rQlGcU-5oEfKR-1wYXr",
-    },
-  };
-
-  const res = await fetch(url, options);
-  const data = await res.json();
-  console.log('Data: ', data.results);
+  const data = await nowPlaying();
 
   return (
     <>
       <section className="grid grid-cols-4 gap-4 p-10 ">
         {data.results.map((movie: Movie) => {
           return (
-            <Link href={"/movie"}>
-              <div className="flex flex-col h-80 w-46 rounded shadow-md bg-slate-200 bg-opacity-20 justify-center items-center cursor-pointer ">
+            <Link href={{
+              pathname: `/movie`,
+              query: {
+                search: `${movie.id}`
+              }
+            }}>
+              <div 
+                  className="flex flex-col h-80 w-46 rounded shadow-md bg-slate-200 bg-opacity-20 justify-center items-center cursor-pointer "
+  
+                >
                 <p className="mb-4 p-2 text-center text-sm font-semibold">
                   {movie.original_title}
                 </p>
